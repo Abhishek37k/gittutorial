a8f7bb98d433115aaf694ffcea4e293d5cbc910a
Abhishek37k <iabhishek6kumar@gmail.com>
2025-02-05 22:57:46 +0530

    Understanding DOM & getElementById Method

---
+++ index.js
@@ -0,0 +1,11 @@
+// Write your code below:
+const header1 = document.getElementById('main-heading');
+header1.textContent = 'Fruit World';
+header1.style.color = 'orange';
+const headerDiv = document.getElementById('header');
+headerDiv.style.backgroundColor = 'green';
+headerDiv.style.borderBottom = '3px solid orange';
+const basketHeading = document.getElementById('basket-heading'); 
+basketHeading.style.color = 'green';
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
